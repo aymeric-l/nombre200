0926bb64d114f200f7d0d72d0ae593e4c5614ae7
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-01-10 13:56:28 +0100

    demande du nombre

--- js/nombre.js
+++ js/nombre.js
@@ -1,1 +1,17 @@
-function
+var nombreSaisi;
+var compteur = 1;
+
+nombreSaisi = prompt("Combien de case voulez-vous ?");
+nombreSaisi = parseInt(nombreSaisi);
+while(compteur != nombreSaisi){
+	clonage();
+	console.log(compteur);
+	compteur++;
+}
+
+
+function clonage() {
+    var item = document.getElementById("liste").lastChild;
+    var clonage = item.cloneNode(true);
+    document.getElementById("liste").appendChild(clonage);
+}
